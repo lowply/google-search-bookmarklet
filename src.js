2a685940bc21bd5457ad879d906343fdb7c2ffea
Sho Mizutani <sho@svifa.net>
2024-08-05 00:20:36 +0900

    A little bit of refactoring

--- src.js
+++ src.js
@@ -1,32 +1,22 @@
-if (window.location.host != "www.google.com") {
-    return false
-}
+if (window.location.host !== "www.google.com") return false;
+if (!window.location.search.includes("q=")) return false;
 
 const href = window.location.href
 const lr = href.match(/lr=[^&]+/)
 const hl = href.match(/hl=[^&]+/)
 
-if (!window.location.search.match(/q=[^&]+/)) {
-    return false
-}
-
 let newhref = href
 
 if (!lr) {
-    newhref += "&lr=lang_ja"
-} else if (lr[0].split("=")[1] == "lang_ja") {
-    newhref = newhref.replace(/lr=[^&]+/, "lr=lang_en")
+    newhref += "&lr=lang_en"
 } else {
-    newhref = newhref.replace(/lr=[^&]+/, "lr=lang_ja")
+    newhref = newhref.replace(/lr=[^&]+/, lr[0].split("=")[1] == "lang_ja" ? "lr=lang_en" : "lr=lang_ja")
 }
 
 if (!hl) {
-    newhref += "&hl=ja"
-} else if (hl[0].split("=")[1] == "ja") {
-    newhref = newhref.replace(/hl=[^&]+/, "hl=en")
+    newhref += "&hl=en";
 } else {
-    newhref = newhref.replace(/hl=[^&]+/, "hl=ja")
+    newhref = newhref.replace(/hl=[^&]+/, hl[0].split("=")[1] === "ja" ? "hl=en" : "hl=ja");
 }
 
 window.location.assign(newhref)
-
